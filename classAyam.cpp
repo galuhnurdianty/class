83be5e9179104603f9e922af40621e9ccdeb30d7
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-01-19 00:19:27 +0800

    membuat tipe data

--- classAyam.cpp
+++ classAyam.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 class ayam{
 	public:
-		
+	void bersuara(string bunyi);
+	void telur(int jmlh);
+	void makanan(string makan);
+	void harga(string dijual);
+		string warnaBulu;
+		string jenisAyam;
 };
